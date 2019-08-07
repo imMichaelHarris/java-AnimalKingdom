8df7c6dc7eb0c22fb3e67a329e2fdeffdba40116
Michael Harris <mhguitar1997@gmail.com>
2019-08-07 15:10:14 -0700

    Get name getter add

--- src/animalKingdom/Animal.java
+++ src/animalKingdom/Animal.java
@@ -1,47 +1,31 @@
 package animalKingdom;
 
 public abstract class Animal {
-    private static int maxId;
+    private static int maxId = 0;
     private int id;
-    private int energy;
     //had these as private gave error, when switched when switched eror went away
     protected String name;
     protected int year;
 
     //constructor every animal will have this
-    public Animal(){
-        energy = 0;
-    }
-    // public Animal(String name, int year){
-    //     maxId++;
-    //     id = maxId;
-    //     energy = 0;
-    //     this.name = name;
-    //     this.year = year;
-    // }
-    //this constructor taks a parameter so we creating a new object 
-    //the animal will have a engery level already
-    // public Animal(String name, int year, int energy){
-    //     maxId++;
-    //     id = maxId;
-    //     this.name = name;
-    //     this.year = year;
-    //     this.energy = energy;
-    // }
 
-    public Animal(int energy){
-        this.energy = energy;
+    public Animal(String name, int year){
+        id = maxId++;
+        this.name = name;
+        this.year = year;
     }
+
 
     //abstract methods all animals can do this be it will vary
-    // public abstract void move();
-    // public abstract void breath();
-    // public abstract void reproduce();
+    public abstract String move();
+    public abstract String breath();
+    public abstract String reproduce();
+    public int getYear(){
+        return year;
+    }
+    public String getName(){
+        return name;
+    }
 
-    public void eat(){
-        energy++;
-    }
-    public void eat(int quanity){
-        energy += quanity;
-    }
+
 }
--- src/animalKingdom/Mammal.java
+++ src/animalKingdom/Mammal.java
@@ -2,10 +2,25 @@ package animalKingdom;
 
 public class Mammal extends Animal{
 
-    public Mammal(String name, int year, int energy){
-        super(energy);
+    public Mammal(String name, int year){
+        super(name, year);
         this.name = name;
         this.year = year;
-
     }
+
+    @Override
+    public String move(){
+        return "walk";
+    }
+
+    @Override
+    public String breath(){
+        return "lungs";
+    }
+
+    @Override
+    public String reproduce(){
+        return "live";
+    }
+
 }
