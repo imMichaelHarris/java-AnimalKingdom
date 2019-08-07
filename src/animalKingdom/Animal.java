5a0c53079c326028546946d68fbad19251f243e0
Michael Harris <mhguitar1997@gmail.com>
2019-08-07 12:52:09 -0700

    Add animal class constructor

--- src/animalKingdom/Animal.java
+++ src/animalKingdom/Animal.java
@@ -4,4 +4,9 @@ public abstract class Animal {
     private static int id;
     private String name;
     private int year;
+
+    //constructor every animal will have this
+    public Animal(){
+        energy = 0;
+    }
 }
