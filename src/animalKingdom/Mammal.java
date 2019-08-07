d6e426eb8b346917bac87286d6545124ad47085b
Michael Harris <mhguitar1997@gmail.com>
2019-08-07 13:22:27 -0700

    Making a new animal class with mammal

--- src/animalKingdom/Mammal.java
+++ src/animalKingdom/Mammal.java
@@ -1,5 +1,11 @@
 package animalKingdom;
 
 public class Mammal extends Animal{
-    
+
+    public Mammal(String name, int year, int energy){
+        super(energy);
+        this.name = name;
+        this.year = year;
+
+    }
 }
