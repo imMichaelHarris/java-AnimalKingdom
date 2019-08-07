abee15e9a801e82a9d24ef8c27049259f5959ee6
Michael Harris <mhguitar1997@gmail.com>
2019-08-07 15:24:49 -0700

    Create bird class

---
+++ src/animalKingdom/Bird.java
@@ -0,0 +1,9 @@
+package animalKingdom;
+
+public class Bird extends Animal{
+    public Bird(String name, int year){
+        super(name, year);
+        this.name = name;
+        this.year = year;
+    }
+}
