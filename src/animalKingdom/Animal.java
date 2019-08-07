f653378f15fffcf33fdc3f1a4168fb5c587175dc
Michael Harris <mhguitar1997@gmail.com>
2019-08-07 13:32:22 -0700

    Animal had default energy

--- src/animalKingdom/Animal.java
+++ src/animalKingdom/Animal.java
@@ -9,27 +9,34 @@ public abstract class Animal {
     protected int year;
 
     //constructor every animal will have this
-    public Animal(String name, int year){
-        maxId++;
-        id = maxId;
+    public Animal(){
         energy = 0;
-        this.name = name;
-        this.year = year;
     }
+    // public Animal(String name, int year){
+    //     maxId++;
+    //     id = maxId;
+    //     energy = 0;
+    //     this.name = name;
+    //     this.year = year;
+    // }
     //this constructor taks a parameter so we creating a new object 
     //the animal will have a engery level already
-    public Animal(String name, int year, int energy){
-        maxId++;
-        id = maxId;
-        this.name = name;
-        this.year = year;
+    // public Animal(String name, int year, int energy){
+    //     maxId++;
+    //     id = maxId;
+    //     this.name = name;
+    //     this.year = year;
+    //     this.energy = energy;
+    // }
+
+    public Animal(int energy){
         this.energy = energy;
     }
 
     //abstract methods all animals can do this be it will vary
-    public abstract void move();
-    public abstract void breath();
-    public abstract void reproduce();
+    // public abstract void move();
+    // public abstract void breath();
+    // public abstract void reproduce();
 
     public void eat(){
         energy++;
